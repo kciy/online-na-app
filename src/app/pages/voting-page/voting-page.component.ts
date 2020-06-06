7c4951b40a1970cc698d14ff3daf55d156d2c74c
viki <viki@vsc.rsvx.dev.it>
2020-06-06 19:13:05 +0200

    add voting page template

---
+++ src/app/pages/voting-page/voting-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voting-page',
+  templateUrl: './voting-page.component.html',
+  styleUrls: ['./voting-page.component.scss']
+})
+export class VotingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
