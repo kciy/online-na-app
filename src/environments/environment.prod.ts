f9137fa903b2ac5dc780733e0a5ec661cbcbeb91
viki <viki@vsc.rsvx.dev.it>
2020-06-07 19:32:57 +0200

    add service worker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAv9hQBWoVjgbjwzcZ5RwdYK2d-3jeaxbc",
+    authDomain: "on-air2020.firebaseapp.com",
+    databaseURL: "https://on-air2020.firebaseio.com",
+    projectId: "on-air2020",
+    storageBucket: "on-air2020.appspot.com",
+    messagingSenderId: "1024117313329",
+    appId: "1:1024117313329:web:3e94290dcd3786422bea10",
+    measurementId: "G-L4BXTPRV2C"
+  }
 };
