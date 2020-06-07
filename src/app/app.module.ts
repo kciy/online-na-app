bdf445368e7a059ec5e2c49e65994fc8e747a815
viki <viki@vsc.rsvx.dev.it>
2020-06-07 19:55:13 +0200

    level pages up one layer, remove PagesModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,22 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { LivePageComponent } from './pages/live-page/live-page.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { PagesModule } from './pages/pages.module';
 import { FormsModule } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
-import { VotingPageComponent } from './pages/voting-page/voting-page.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    LivePageComponent,
-    NotFoundPageComponent,
-    VotingPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    PagesModule,
+    AppRoutingModule,
     MatMenuModule,
     MatCardModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
