409ce2e1db63e100247470f34c8bff269756904d
viki <viki@vsc.rsvx.dev.it>
2020-06-10 12:32:49 +0200

    add todo-list on home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
@@ -8,12 +9,14 @@ import { MatTabsModule } from '@angular/material/tabs';
     imports: [
         CommonModule,
         MatCardModule,
+        MatDividerModule,
         MatExpansionModule,
         MatTabsModule
     ],
     providers: [],
     exports: [
         MatCardModule,
+        MatDividerModule,
         MatExpansionModule,
         MatTabsModule
     ],
