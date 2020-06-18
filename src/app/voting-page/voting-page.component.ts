c350d51c27a21cd8236cf1117d6c1a3b4cc7de85
Lukas Heddedndorp <lu.heddendorp@gmail.com>
2020-06-18 10:58:39 +0200

    Update for the voting info page

--- src/app/voting-page/voting-page.component.ts
+++ src/app/voting-page/voting-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-declare function voting(): any;
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, timer} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-voting-page',
   templateUrl: './voting-page.component.html',
   styleUrls: ['./voting-page.component.scss']
 })
-export class VotingPageComponent implements OnInit {
-  constructor() {}
+export class VotingPageComponent {
+  results$: Observable<Result[]>;
+  lastUpdate$: Observable<Date>;
 
-  ngOnInit() {
-    voting();
+  constructor(http: HttpClient) {
+    const resultRequest = http.get<any[]>('https://vote.esn-germany.de/results').pipe(map(res => res.map(result => ({
+      ...result,
+      anonymized: result.anonymized === 'Y',
+      active: result.active === 'Y',
+      progress: (result.total / result.vote) * 100
+    }))));
+    this.results$ = timer(0, 10000).pipe(switchMap(() => resultRequest));
+    this.lastUpdate$ = this.results$.pipe(map(() => new Date()));
   }
+}
+
+interface Result {
+  id: number;
+  anonymized: boolean;
+  active: boolean;
+  name: string;
+  vote: number;
+  total: number;
 }
