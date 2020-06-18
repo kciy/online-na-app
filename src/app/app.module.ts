c7b9b645287d403de19f30755496331d0c790996
Lukas Heddedndorp <lu.heddendorp@gmail.com>
2020-06-18 10:58:39 +0200

    Update for the voting info page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { SharedModule } from './shared/shared.module';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LivePageComponent } from './live-page/live-page.component';
-import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
-import { VotingPageComponent } from './voting-page/voting-page.component';
-import { DocumentsPageComponent } from './documents-page/documents-page.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AgendaExplorerComponent } from './live-page/agenda-explorer/agenda-explorer.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {SharedModule} from './shared/shared.module';
+import {HomePageComponent} from './home-page/home-page.component';
+import {LivePageComponent} from './live-page/live-page.component';
+import {NotFoundPageComponent} from './not-found-page/not-found-page.component';
+import {VotingPageComponent} from './voting-page/voting-page.component';
+import {DocumentsPageComponent} from './documents-page/documents-page.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AgendaExplorerComponent} from './live-page/agenda-explorer/agenda-explorer.component';
 
 @NgModule({
   imports: [
@@ -21,9 +21,9 @@ import { AgendaExplorerComponent } from './live-page/agenda-explorer/agenda-expl
     AppRoutingModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
