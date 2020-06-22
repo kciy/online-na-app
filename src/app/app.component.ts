ba14bd653312f7456ccc467a4acfb4fe070699b3
viki <viki@vsc.rsvx.dev.it>
2020-06-22 18:49:16 +0200

    add darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ThemeService } from './theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,49 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'online-na-app';
   
-  constructor() {}
+  constructor(
+    private themeService: ThemeService
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.toggleTheme();
+    this.toggleTheme();
+    // this.toggleTheme();
+  }
 
   ngOnDestroy(): void {}
+
+  toggleTheme() {
+    if (this.themeService.isDarkTheme()) {
+      this.themeService.setLightTheme();
+    } else {
+      this.themeService.setDarkTheme();
+    }
+    var button = document.getElementById("toggleButton");
+    // var icon = document.createElement("mat-icon");
+    // icon.classList.add("icon-right");
+    // icon.innerHTML = "highlight";
+
+    if (button.innerHTML === "Light Mode") {
+      button.innerHTML = "Dark Mode";
+      // button.appendChild(icon);
+    } else {
+      button.innerHTML = "Light Mode";
+      // button.appendChild(icon);
+    }
+
+    var footer = document.getElementById("footer-img");
+    if (footer.getAttribute('src') === "../assets/logo/DE_colour.png") {
+      footer.setAttribute('src', "../assets/logo/DE_colour-white.png");
+    } else {
+      footer.setAttribute('src', "../assets/logo/DE_colour.png");
+    }
+
+    var img = document.getElementById("agenda-img");
+    if (img.getAttribute('src') === "../../../assets/icons/calendar-month.png") {
+      img.setAttribute('src', "../../../assets/icons/calendar-month-white.png");
+    } else {
+      img.setAttribute('src', "../../../assets/icons/calendar-month.png");
+    }
+  }
 }
--- src/app/live-page/live-page.component.ts
+++ src/app/live-page/live-page.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-live-page',
   templateUrl: './live-page.component.html',
-  styleUrls: ['./live-page.component.scss']
+  styleUrls: ['./live-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LivePageComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const stream = document.getElementById("youtube-link");
+    if (moment().isAfter('2020-06-28', 'day')) {
+      stream.setAttribute("src", "https://www.youtube.com/embed/3YjLgjJIZ6o");
+    }
+  }
 
 }
