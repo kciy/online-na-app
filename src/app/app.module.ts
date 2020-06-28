4dea681432ad47b77b3463c6b840337060968c33
Lukas Heddedndorp <lu.heddendorp@gmail.com>
2020-06-28 09:20:13 +0200

    Add voting results display
    + Adds the csv files that contain the results
    + Adds a parser to transform those files into a more usable format
    + Adds UI to display the results on desktop and mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {SharedModule} from './shared/shared.module';
-import {HomePageComponent} from './home-page/home-page.component';
-import {LivePageComponent} from './live-page/live-page.component';
-import {NotFoundPageComponent} from './not-found-page/not-found-page.component';
-import {VotingPageComponent} from './voting-page/voting-page.component';
-import {DocumentsPageComponent} from './documents-page/documents-page.component';
-import {HttpClientModule} from '@angular/common/http';
-import {AgendaExplorerComponent} from './live-page/agenda-explorer/agenda-explorer.component';
-import {AngularFireModule} from "@angular/fire";
-import {AngularFireAnalyticsModule, ScreenTrackingService} from "@angular/fire/analytics";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LivePageComponent } from './live-page/live-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { VotingPageComponent } from './voting-page/voting-page.component';
+import { DocumentsPageComponent } from './documents-page/documents-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AgendaExplorerComponent } from './live-page/agenda-explorer/agenda-explorer.component';
+import { AngularFireModule } from '@angular/fire';
+import {
+  AngularFireAnalyticsModule,
+  ScreenTrackingService,
+} from '@angular/fire/analytics';
 import { FooterComponent } from './footer/footer.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
@@ -25,8 +29,11 @@ import { FooterComponent } from './footer/footer.component';
     AngularFireAnalyticsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
     SharedModule,
+    FlexLayoutModule,
     AppRoutingModule,
   ],
   declarations: [
@@ -40,6 +47,6 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent,
   ],
   providers: [ScreenTrackingService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
