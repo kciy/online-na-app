0d83e85117b2e629ece7323ecb9ba300a07aa390
viki <viki@vsc.rsvx.dev.it>
2020-09-11 16:47:16 +0200

    Update to Vol.2 and add Registration link

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
+  preserveWhitespaces: true,
 })
 export class HomePageComponent implements OnInit {
   constructor() {}
